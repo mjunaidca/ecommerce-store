81f3248047e35d368ec2be90039c030cb7a8e612
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-08-18 11:17:50 +0500

    Update Cart function on Product Page

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -5,12 +5,18 @@ import { ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { Product } from "@/type";
-// import useCart from "@/hooks/use-cart";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -34,9 +40,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button 
-        // onClick={onAddToCart} 
-        className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart size={20} />
         </Button>
